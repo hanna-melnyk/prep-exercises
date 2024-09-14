67eab5c784029623d13ddd74c698fff9f10308cf
Hanna <hanna.melnk@gmail.com>
2024-09-14 12:49:34 +0200

    prep ex1 and link to ex2

---
+++ React/week3/prep-exercises/1-check-screen-size/check-screen-size/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {WindowComponent} from "./components/windowSizeComponent";
+
+export const App = () => {
+  return (
+      <div>
+        <h1>Responsive Background Color App</h1>
+        <WindowComponent />
+      </div>
+  );
+};
